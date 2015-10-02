3301a1542a915119159d88543808af4fa6e29b83
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-10-02 08:30:14 -0400

    Queue fields and methods

--- server/queue.go
+++ server/queue.go
@@ -1,7 +1,33 @@
 package main
 
+type Message struct {
+	header *amqp.ContentHeaderFrame
+	payload []amqp.WireFrame
+}
+
 type Queue struct {
+	name string
+	passive bool
+	durable bool
+	exclusive bool
+	autoDelete bool
+	noWait bool
+	arguments Table
+	queue chan Message
+}
+
+func (q *Queue) add(message Message) {
+	q.queue <- message
 }
 
 type Binding struct {
+	queueName string
+	exchangeName string
+	routingKey string
+	noWait bool
+	arguments Table
+}
+
+func (b *Binding) matches(method *amqp.BasicPublish) bool {
+	return false
 }
