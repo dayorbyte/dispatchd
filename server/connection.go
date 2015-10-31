468aa217271339ab581e57ca913e11ca4435f738
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-10-31 14:12:08 -0400

    Add client properties of connections and channel count

--- server/connection.go
+++ server/connection.go
@@ -43,9 +43,21 @@ type AMQPConnection struct {
 }
 
 func (conn *AMQPConnection) MarshalJSON() ([]byte, error) {
+	cProps := make(map[string]interface{})
+	for name, value := range *conn.clientProperties {
+		switch v := value.(type) {
+		case []byte:
+			cProps[name] = string(v)
+		default:
+			cProps[name] = v
+		}
+	}
+
 	return json.Marshal(map[string]interface{}{
-		"id":      conn.id,
-		"address": fmt.Sprintf("%s", conn.network.RemoteAddr()),
+		"id":               conn.id,
+		"address":          fmt.Sprintf("%s", conn.network.RemoteAddr()),
+		"clientProperties": cProps,
+		"channelCount":     len(conn.channels),
 	})
 }
 
