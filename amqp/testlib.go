35ad0812d357e1f3ae33bb23519eca6632ddb660
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-11-13 07:32:29 -0500

    amqp testlib. generate random messages for using in tests

---
+++ amqp/testlib.go
@@ -0,0 +1,69 @@
+package amqp
+
+import (
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
+
+var counter int64
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	counter = 0
+}
+
+func nextId() int64 {
+	return atomic.AddInt64(&counter, 1)
+}
+
+func RandomMessage(persistent bool) *Message {
+	var size, payload = RandomPayload()
+	var randomHeader = RandomHeader(size, persistent)
+	var exchange = "exchange-name"
+	var routingKey = "routing-key"
+	var randomPublish = &BasicPublish{
+		Exchange:   exchange,
+		RoutingKey: routingKey,
+		Mandatory:  false,
+		Immediate:  false,
+	}
+	return &Message{
+		Id:          nextId(),
+		Header:      randomHeader,
+		Payload:     payload,
+		Exchange:    exchange,
+		Key:         routingKey,
+		Method:      randomPublish,
+		Redelivered: 0,
+		LocalId:     nextId(),
+	}
+}
+
+func RandomPayload() (uint64, []*WireFrame) {
+	var size uint64 = 500
+	var payload = make([]byte, size)
+	return size, []*WireFrame{
+		&WireFrame{
+			FrameType: 3,
+			Channel:   1,
+			Payload:   payload,
+		},
+	}
+}
+
+func RandomHeader(size uint64, persistent bool) *ContentHeaderFrame {
+	var props = BasicContentHeaderProperties{}
+	if persistent {
+		*props.DeliveryMode = byte(2)
+	} else {
+		*props.DeliveryMode = byte(1)
+	}
+	return &ContentHeaderFrame{
+		ContentClass:    60,
+		ContentWeight:   0,
+		ContentBodySize: size,
+		PropertyFlags:   0,
+		Properties:      &props,
+	}
+}
