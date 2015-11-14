0a4479b37880bac8356d568a610de8a1fa56cbf5
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-11-14 10:53:53 -0500

    msgstore test file

---
+++ msgstore/msgstore_test.go
@@ -0,0 +1,57 @@
+package msgstore
+
+import (
+	// "container/list"
+	"fmt"
+	"github.com/jeffjenkins/mq/amqp"
+	"github.com/jeffjenkins/mq/interfaces"
+	"os"
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+	// Setup
+	var dbFile = "TestWrite.db"
+	os.Remove(dbFile)
+	rhs := []interfaces.MessageResourceHolder{&TestResourceHolder{}}
+	ms, err := NewMessageStore(dbFile)
+	// Create messages
+	msg1 := amqp.RandomMessage(true)
+	msg2 := amqp.RandomMessage(true)
+	fmt.Printf("Creating ids: %d, %d\n", msg1.Id, msg2.Id)
+
+	// Store messages and delete one
+	ms.AddMessage(msg1, []string{"some-queue", "some-other-queue"})
+	qm2Map, err := ms.AddMessage(msg2, []string{"some-queue"})
+	_, err = ms.GetAndDecrRef(qm2Map["some-queue"][0], "some-queue", rhs)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	// Close DB
+	ms.db.Close()
+	keys := map[int64]bool{
+		msg1.Id: true,
+		// msg2.Id: true,
+	}
+	// Assert that the DB is correct
+	err = assertKeys(dbFile, keys)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	// try loading from disk in the message store
+	ms2, err := NewMessageStore(dbFile)
+	_, err = ms2.LoadQueueFromDisk("some-queue")
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	_, err = ms2.LoadQueueFromDisk("some-other-queue")
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+}
---
+++ msgstore/testlib.go
@@ -0,0 +1,93 @@
+package msgstore
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/jeffjenkins/mq/amqp"
+	"reflect"
+)
+
+// ids to map
+func idsToMap() {
+
+}
+
+// Check if the msg store is composed of exactly
+func assertKeys(dbName string, keys map[int64]bool) error {
+	// Open DB
+	db, err := bolt.Open(dbName, 0600, nil)
+	defer db.Close()
+	if err != nil {
+		return err
+	}
+	err = db.View(func(tx *bolt.Tx) error {
+		// Check index
+		bucket := tx.Bucket([]byte("message_index"))
+		if bucket == nil {
+			return nil
+		}
+
+		// get from db
+		var indexKeys, err1 = keysForBucket(tx, "message_index")
+		var contentKeys, err2 = keysForBucket(tx, "message_contents")
+		if err1 != nil {
+			return err1
+		}
+		if err2 != nil {
+			return err2
+		}
+
+		// Check equality
+		// TODO: return key diff
+		indexNotKeys := subtract(indexKeys, keys)
+		keysNotIndex := subtract(keys, indexKeys)
+
+		if !reflect.DeepEqual(keys, indexKeys) {
+			return fmt.Errorf("Different values in index!\nindexNotKeys:%q\nkeysNotIndex:%q", indexNotKeys, keysNotIndex)
+		}
+		if !reflect.DeepEqual(keys, contentKeys) {
+			return fmt.Errorf("Different values in content!")
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func subtract(original map[int64]bool, subtractThis map[int64]bool) []int64 {
+	ret := make([]int64, 0)
+	for id, _ := range original {
+		_, found := subtractThis[id]
+		if !found {
+			ret = append(ret, id)
+		}
+	}
+	return ret
+}
+
+func keysForBucket(tx *bolt.Tx, bucketName string) (map[int64]bool, error) {
+	// Check index
+	bucket := tx.Bucket([]byte(bucketName))
+	if bucket == nil {
+		return nil, fmt.Errorf("No bucket!")
+	}
+	var cursor = bucket.Cursor()
+	var keys = make(map[int64]bool)
+	for bid, _ := cursor.First(); bid != nil; bid, _ = cursor.Next() {
+		fmt.Printf("%s, key:%d\n", bucketName, bytesToInt64(bid))
+		keys[bytesToInt64(bid)] = true
+	}
+	return keys, nil
+}
+
+type TestResourceHolder struct {
+}
+
+func (trh *TestResourceHolder) AcquireResources(qm *amqp.QueueMessage) bool {
+	return true
+}
+func (trh *TestResourceHolder) ReleaseResources(qm *amqp.QueueMessage) {
+
+}
