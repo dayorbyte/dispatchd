975d6cae85af0b8ab0154dd0d6eea67eacc362d9
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-11-29 14:53:00 -0500

    Helpers to log all values sent to/from the server

--- server/server_test.go
+++ server/server_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	// "fmt"
+	"encoding/json"
 	"github.com/jeffjenkins/dispatchd/amqp"
 	"github.com/jeffjenkins/dispatchd/util"
 	"net"
@@ -43,7 +44,34 @@ func methodToWireFrame(channelId uint16, method amqp.MethodFrame) *amqp.WireFram
 	return &amqp.WireFrame{uint8(amqp.FrameMethod), channelId, buf.Bytes()}
 }
 
-func testServerHelper(path string) (s *Server, toServer chan *amqp.WireFrame, fromServer chan *amqp.WireFrame) {
+func wireFrameToMethod(frame *amqp.WireFrame) amqp.MethodFrame {
+	var methodReader = bytes.NewReader(frame.Payload)
+	var methodFrame, err = amqp.ReadMethod(methodReader)
+	if err != nil {
+		panic("Failed to read method from server. Maybe not a method?")
+	}
+	return methodFrame
+}
+
+func logResponse(t *testing.T, fromServer chan *amqp.WireFrame) {
+	var method = wireFrameToMethod(<-fromServer)
+	var js, err = json.Marshal(method)
+	if err != nil {
+		panic("could not encode json, testing error!")
+	}
+	t.Logf(">>>> RECV '%s': %s", method.MethodName(), string(js))
+}
+
+func sendAndLogMethod(t *testing.T, channelId uint16, toServer chan *amqp.WireFrame, method amqp.MethodFrame) {
+	var js, err = json.Marshal(method)
+	if err != nil {
+		panic("")
+	}
+	t.Logf("<<<< SENT '%s': %s", method.MethodName(), string(js))
+	toServer <- methodToWireFrame(channelId, method)
+}
+
+func testServerHelper(t *testing.T, path string) (s *Server, toServer chan *amqp.WireFrame, fromServer chan *amqp.WireFrame) {
 	// Make channels
 	// TODO: reduce these once we're reading/writng to the server
 	toServer = make(chan *amqp.WireFrame, 500)
@@ -59,42 +87,52 @@ func testServerHelper(path string) (s *Server, toServer chan *amqp.WireFrame, fr
 	go s.openConnection(internal)
 	// Set up connection
 	external.Write([]byte{'A', 'M', 'Q', 'P', 0, 0, 9, 1})
-	toServer <- methodToWireFrame(0, &amqp.ConnectionStartOk{
+	logResponse(t, fromServer) // start
+	sendAndLogMethod(t, 0, toServer, &amqp.ConnectionStartOk{
 		ClientProperties: amqp.NewTable(),
 		Mechanism:        "PLAIN",
 		Response:         []byte("guest\x00guest"),
 		Locale:           "en_US",
 	})
-	toServer <- methodToWireFrame(0, &amqp.ConnectionTuneOk{})
-	toServer <- methodToWireFrame(0, &amqp.ConnectionOpen{})
+	logResponse(t, fromServer) // tune
+	sendAndLogMethod(t, 0, toServer, &amqp.ConnectionTuneOk{})
+	sendAndLogMethod(t, 0, toServer, &amqp.ConnectionOpen{})
+	logResponse(t, fromServer) // openok
 	return
 }
 
 func TestAddChannel(t *testing.T) {
+	// Setup
 	path := dbPath()
 	defer os.Remove(path)
-	s, toServer, fromServer := testServerHelper(path)
+	s, toServer, fromServer := testServerHelper(t, path)
 	if len(s.conns) != 1 {
 		t.Errorf("Wrong number of open connections: %d", len(s.conns))
 	}
+	// Create channel
 	var chid = uint16(1)
-	toServer <- methodToWireFrame(chid, &amqp.ChannelOpen{})
+	sendAndLogMethod(t, chid, toServer, &amqp.ChannelOpen{})
+	logResponse(t, fromServer)
 
-	toServer <- methodToWireFrame(chid, &amqp.ExchangeDeclare{
+	// Create exchange
+	sendAndLogMethod(t, chid, toServer, &amqp.ExchangeDeclare{
 		Exchange:  "ex-1",
 		Type:      "topic",
-		NoWait:    true,
 		Arguments: amqp.NewTable(),
 	})
+	logResponse(t, fromServer)
 	if len(s.exchanges) != 5 {
 		t.Errorf("Wrong number of exchanges: %d", len(s.exchanges))
 	}
-	toServer <- methodToWireFrame(chid, &amqp.QueueDeclare{
+
+	// Create Queue
+	sendAndLogMethod(t, chid, toServer, &amqp.QueueDeclare{
 		Queue:     "q-1",
 		Arguments: amqp.NewTable(),
 	})
 	if len(s.queues) != 1 {
 		t.Errorf("Wrong number of queues: %d", len(s.queues))
 	}
-	_ = <-fromServer
+	logResponse(t, fromServer)
+
 }
