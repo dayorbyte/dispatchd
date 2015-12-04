0fe20c0e8f4aad54d40770cff78b45eabbf9e6b6
Jeff Jenkins <jeff@jeffreyjenkins.ca>
2015-12-04 08:58:41 -0500

    Test for immediate delivery working

--- server/server_publish_test.go
+++ server/server_publish_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/jeffjenkins/dispatchd/util"
 	"testing"
 )
 
-func TestImmediate(t *testing.T) {
+func TestImmediateFail(t *testing.T) {
 	tc := newTestClient(t)
 	defer tc.cleanup()
 	conn := tc.connect()
@@ -22,6 +23,23 @@ func TestImmediate(t *testing.T) {
 	if string(ret.Body) != string(TEST_TRANSIENT_MSG.Body) {
 		t.Fatalf("Did not get same payload back in BasicReturn")
 	}
+}
+
+func TestImmediate(t *testing.T) {
+	tc := newTestClient(t)
+	defer tc.cleanup()
+	conn := tc.connect()
+	ch, _, _ := channelHelper(tc, conn)
+
+	ch.QueueDeclare("q1", false, false, false, false, NO_ARGS)
+	ch.QueueBind("q1", "abc", "amq.direct", false, NO_ARGS)
+
+	deliveries, err := ch.Consume("q1", util.RandomId(), false, false, false, false, NO_ARGS)
+	if err != nil {
+		t.Fatalf("Failed to consume")
+	}
+	ch.Publish("amq.direct", "abc", false, true, TEST_TRANSIENT_MSG)
+	<-deliveries
 }
 
 func TestMandatory(t *testing.T) {
